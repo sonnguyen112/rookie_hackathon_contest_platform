4c142f4fee5d7f8732a39417d67a5c13ffd534af
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2023-12-27 03:08:20 +0700

    Create Tables

--- src/main/java/com/group10/contestPlatform/entities/User.java
+++ src/main/java/com/group10/contestPlatform/entities/User.java
@@ -1,7 +1,13 @@
 package com.group10.contestPlatform.entities;
 
 import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -23,7 +29,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue
     private Long id;
@@ -46,4 +52,28 @@ public class User {
     private Set<Quiz> createdQuizzes;
     @OneToMany(mappedBy = "user")
     private Set<Take> takes;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).toList();
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
